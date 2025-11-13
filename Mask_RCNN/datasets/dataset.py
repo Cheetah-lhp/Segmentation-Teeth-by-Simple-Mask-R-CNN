ba754d836da367346b38db120b53e45af9c115b8
cheetah-lhp <thphuc16092005@gmail.com>
2025-11-13 17:13:02 +0700

    update processing dataset

---
+++ Mask_RCNN/datasets/dataset.py
@@ -0,0 +1,67 @@
+import torch
+import os
+import json
+import numpy as np
+from PIL import Image
+from generalized_dataset import GeneralizedDataset
+
+class DentalDataset(GeneralizedDataset):
+    def __init__(self, root_dir):
+        super().__init__(max_workers=4, verbose=False)
+        self.root_dir = root_dir
+        self.train = True
+
+        self.img_dir  = os.path.join(root_dir, "Radiographs")
+        self.mask_max_dir = os.path.join(root_dir, "Segmentation", "maxillomandibular")
+        self.mask_teeth_dir = os.path.join(root_dir, "Segmentation", "teeth_mask")
+        self.bbox_json = os.path.join(root_dir, "Segmentation", "teeth_bbox.json")
+
+        #danh sach id anh (loai bo phan duoi .jpg)
+        self.ids = [
+            os.path.splitext(f)[0]
+            for f in os.listdir(self.img_dir) 
+            if f.endswith('.jpg')]
+        
+        with open(os.path.join(self.root_dir, "Segmentation", "teeth_bbox.json")) as f:
+            self.bbox_data = json.load(f)
+
+
+    def get_image(self, img_id):
+        img_path = os.path.join(self.img_dir, f"{img_id}.jpg")
+        image = Image.open(img_path).convert("RGB")
+        return image
+    
+
+
+    def get_target(self, img_id):
+        mask_teeth_path = os.path.join(self.mask_teeth_dir, f"{img_id}.jpg")
+        mask_teeth = Image.open(mask_teeth_path).convert("L")
+        mask_teeth_np = np.array(mask_teeth)
+
+        """xu ly them mask maxillomandibular (vung ham`) (optional)
+        mask_max_path = os.path.join(self.mask_max_dir, f"{img_id}.jpg")
+        mask_max = Image.open(mask_max_path).convert("L")
+        """
+
+        obj_ids = np.unique(mask_teeth_np)
+        obj_ids = obj_ids[1:] #bo qua background class (0)
+
+        #tao mask nhi phan cho tung doi tuong
+        masks = mask_teeth_np == obj_ids[:, None, None]
+
+        #doc bounding boxes tu json
+        objects = self.bbox_data[int(img_id)]["Label"]["objects"]
+        boxes = [obj["bounding box"] for obj in objects]
+        labels = [int(obj["title"]) for obj in objects]
+        
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.ones(labels, dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        target = {
+            "boxes": boxes,
+            "labels": labels,
+            "masks": masks,
+            "image_id": torch.tensor([int(img_id)]),
+        }
+        return target
--- Mask_RCNN/datasets/generalized_dataset.py
+++ Mask_RCNN/datasets/generalized_dataset.py
@@ -1,53 +1,64 @@
-import os
-import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import os #quan ly file
+import time # quan ly thoi gian thuc thi
+from concurrent.futures import ThreadPoolExecutor, as_completed # thuc thi tac vu song song
 
 import torch
 from torchvision import transforms
 
+
 class GeneralizedDataset:
+    
     def __init__(self, max_workers=2, verbose=False):
         self.max_workers = max_workers
         self.verbose = verbose
-
+            
     def __getitem__(self, i):
         img_id = self.ids[i]
+        image = self.get_image(img_id)
         image = transforms.ToTensor()(image)
-        target = self.get_target(img_id) if self.train else{}
-        return image, target
-    
+        target = self.get_target(img_id) if self.train else {}
+        return image, target   
+    """truy cap va kiem tra mot (mau~) phan tu trong dataset tai vi tri i"""
     def __len__(self):
         return len(self.ids)
-    
+    """so luong mau du lieu trong dataset"""
+
     def check_dataset(self, checked_id_file):
+        """
+        use multithreads to accelerate the process.
+        check the dataset to avoid some problems listed in method `_check`.
+        """
+        
         if os.path.exists(checked_id_file):
             info = [line.strip().split(", ") for line in open(checked_id_file)]
             self.ids, self.aspect_ratios = zip(*info)
             return
-        
-        since = time.time()
-        print("checking dataset..")
+        """kiem tra neu da co file kiem tra truoc do"""
 
-        executor  = ThreadPoolExecutor(max_workers=self.max_workers)
+        since = time.time()
+        print("Checking the dataset...")
+        
+        executor = ThreadPoolExecutor(max_workers=self.max_workers)
         seqs = torch.arange(len(self)).chunk(self.max_workers)
         tasks = [executor.submit(self._check, seq.tolist()) for seq in seqs]
-
+        """ chia du lieu thanh nhieu phan (chunk) tuong ung so luong, moi luong goi ham _check() de kiem tra du lieu"""
+        
         outs = []
         for future in as_completed(tasks):
-            outs.extend(future.result())
+            outs.extend(future.result()) #cac anh hop le duoc dua vao outs
         if not hasattr(self, "id_compare_fn"):
             self.id_compare_fn = lambda x: int(x)
-        outs.sort(key=lambda x: self.id_compare_fn(x[0]))
-
+        outs.sort(key=lambda x: self.id_compare_fn(x[0])) # sap xep danh sach theo id anh
+        
         with open(checked_id_file, "w") as f:
             for img_id, aspect_ratio in outs:
                 f.write("{}, {:.4f}\n".format(img_id, aspect_ratio))
-        
+        # ghi ra file ket qua, moi dong: image_id, aspect_ratio
         info = [line.strip().split(", ") for line in open(checked_id_file)]
         self.ids, self.aspect_ratios = zip(*info)
         print("checked id file: {}".format(checked_id_file))
         print("{} samples are OK; {:.1f} seconds".format(len(self), time.time() - since))
-
+        
     def _check(self, seq):
         out = []
         for i in seq:
@@ -66,5 +77,11 @@ class GeneralizedDataset:
             except AssertionError as e:
                 if self.verbose:
                     print(img_id, e)
-
         return out
+    """check 3 dieu kien:
+        1. anh phai co it nhat 1 bbox
+        2. so luong boxes = so luong lables
+        3. so luong boxes = so luong masks
+        => neu hop le them vao out img_id va aspect_ratios
+        => neu k hop le in ra loi (verbose=true)"""
+                    
